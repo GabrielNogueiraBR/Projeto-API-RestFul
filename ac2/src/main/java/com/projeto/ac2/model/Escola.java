c34123ec0a28a3e2592f473f852c7226825544ab
Gabriel Nogueira <gabrielnoog7@gmail.com>
2020-10-28 22:00:59 -0300

    Create model Escola.java

---
+++ ac2/src/main/java/com/projeto/ac2/model/Escola.java
@@ -0,0 +1,18 @@
+package com.projeto.ac2.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Escola {
+    
+    private int idEscola;
+    private String nomeEscola;
+    private String rua;
+    private String cidade;
+    private String estado;
+    private LocalDate dataFundacao;
+    private List<Curso> listaCursos;
+
+    
+    
+}
