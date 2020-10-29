ef0dfc15abe3e434b7163e63b4747767772d310b
Gabriel Nogueira <gabrielnoog7@gmail.com>
2020-10-28 22:24:58 -0300

    Definição de atributos e metodos da classe Escola.java

--- ac2/src/main/java/com/projeto/ac2/model/Escola.java
+++ ac2/src/main/java/com/projeto/ac2/model/Escola.java
@@ -1,6 +1,8 @@
 package com.projeto.ac2.model;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Escola {
@@ -11,7 +13,81 @@ public class Escola {
     private String cidade;
     private String estado;
     private LocalDate dataFundacao;
-    private List<Curso> listaCursos;
+    private List<Curso> listaCursos = new ArrayList<Curso>();
+
+    //Sobreposicao de metodos construtores
+    public Escola(){
+
+    }
+
+    public Escola(int idEscola, String nomeEscola, String rua, String cidade, String estado, LocalDate dataFundacao) {
+        this.idEscola = idEscola;
+        this.nomeEscola = nomeEscola;
+        this.rua = rua;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.dataFundacao = dataFundacao;
+    }
+
+    public String getNomeEscola() {
+        return nomeEscola;
+    }
+
+    public void setNomeEscola(String nomeEscola) {
+        this.nomeEscola = nomeEscola;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public LocalDate getDataFundacao() {
+        return dataFundacao;
+    }
+
+    public void setDataFundacao(LocalDate dataFundacao) {
+        this.dataFundacao = dataFundacao;
+    }
+
+    //Primeira forma de adicionar um curso
+    public Boolean addCurso(Curso curso){
+        return listaCursos.add(curso);
+    }
+
+    //Segunda forma de adicionar um curso
+    public Boolean addCurso(int idCurso, String nomeCurso, String descricao, String nomeProfessorCoordenador, LocalTime cargaHoraria, int alunosMatriculados, Escola escola){
+        return listaCursos.add(new Curso(idCurso, nomeCurso, descricao, nomeProfessorCoordenador, cargaHoraria, alunosMatriculados, escola));
+    }
+    
+    public Boolean removeCurso(Curso curso){
+        return listaCursos.remove(curso);
+    }
+
+    @Override
+    public String toString() {
+        return "\n\nNome da Escola:" + nomeEscola + "\nData de Fundacao = " + dataFundacao + "\nRua = " + rua + "\nCidade = " + cidade  + "\nEstado = " + estado +  "\nidEscola = "
+                + idEscola + "\nLista de Cursos = " + listaCursos + "\n\n";
+    }
 
     
     
